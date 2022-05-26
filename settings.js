32dc6777d273c4a216c9145c270f4e09e1b2cb94
Kenichi-BOTZ <106317066+Kenichi-BOTZ@users.noreply.github.com>
2022-05-26 21:21:33 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['62895388375000','62895388375000','62895388375000'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['62895388375000'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '62895388375000' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'KENICHI' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'KeniBotz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Kenichi' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://www.youtube.com/channel/UCf16UupgKYIvhb-o7GItHGQ'
+global.ig = 'https://instagram.com/kenibotz_kenichi'
+global.mygc = 'https://chat.whatsapp.com/Ipy7Jjgvq3FKhIMCjvjX1N'
+global.myweb = 'wa.me/62895388375000'
+global.email = 'kenichiytch@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© KeniBotz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Kenichi' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 25,
 }
 
 //—————「 Set Image 」—————//
